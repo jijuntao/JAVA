57309926c793a64cf52f06e9f14893d785db8dce
jijuntao <18855532272@163.com>
2019-09-17 23:54:48 +0800

    java第一课helloworld

---
+++ src/Java_study/HelloWorld.java
@@ -0,0 +1,12 @@
+package Java_study;
+
+public class HelloWorld {
+    //类的声明
+    //代码必须写在类中
+    public static void main(String[] args){
+        int a = 1;
+        int b = 2;
+     //输出
+        System.out.println(a+b);
+    }
+}
